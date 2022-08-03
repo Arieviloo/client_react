4426d38e4bf24d9ca204735c00e9a823fd15ddb1
Jadiê Oliveira <jadieoliveira00@gmail.com>
2022-08-03 17:36:32 -0300

    Heading

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
